e0747bf7d58b9d77f5796b8c7ed9737a85a365df
Marc Pons Serrano <marc.pons.serrano@everis.com>
2022-08-09 12:12:13 +0200

    chore: intial set up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
-import { NgModule } from '@angular/core';
+import { DoBootstrap, Injector, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { createCustomElement } from '@angular/elements';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -11,6 +11,16 @@ import { AppComponent } from './app.component';
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  entryComponents: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule implements DoBootstrap {
+
+  constructor(private injector: Injector){}
+
+  ngDoBootstrap(): void {
+    const customElement = createCustomElement(AppComponent, { injector:this.injector });
+    console.log("doBootstrap")
+    customElements.define('angular-web-component-v14', customElement);
+  }
+}
